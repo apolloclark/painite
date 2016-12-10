90c91b84d04f20c281d13e2bce44bf29150a0fe9
Apollo Clark <apolloclark@gmail.com>
2016-12-09 19:05:45 -0500

    bundle only install

--- install_gauntlt.sh
+++ install_gauntlt.sh
@@ -4,15 +4,4 @@ apt-get update
 apt-get install -y locate
 updatedb
 
-# install RVM, source, install Ruby 2.3.0
-gpg --keyserver hkp://keys.gnupg.net --recv-keys \
-    409B6B1796C275462A1703113804BB82D39DC0E3
-curl -sSL https://get.rvm.io | bash -s stable
-source /etc/profile.d/rvm.sh
-rvm requirements
-rvm use 2.3.0 --default --install --fuzzy
-
-cd ~
-git clone https://github.com/gauntlt/gauntlt
-cd gauntlt
-bundle install
+gem install gauntlt --no-rdoc --no-ri
